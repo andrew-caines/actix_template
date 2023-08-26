46e02d96a6972b7fe2460d57eec465cd61572d51
Andrew Caines <andrew.p.caines@gmail.com>
2023-08-26 17:24:27 -0600

    Started to flesh out db setup and base requirements for cargo.toml

--- src/state.rs
+++ src/state.rs
@@ -1,8 +1,10 @@
-use std::sync::Mutex;
 use chrono::{DateTime, Utc};
+use sqlx::postgres::PgPool;
+use std::sync::Mutex;
 
 pub struct AppState {
-   pub application_name: String,
-   pub health_check_count: Mutex<i32>,
-   pub last_check_time: Mutex<DateTime<Utc>>,
+    pub application_name: String,
+    pub health_check_count: Mutex<i32>,
+    pub last_check_time: Mutex<DateTime<Utc>>,
+    pub pg_db: PgPool,
 }
